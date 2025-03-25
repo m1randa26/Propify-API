bc81b88e8b82234ca32531fc2e769abd41278c6d
Isaac <jim.isaacmiranda@gmail.com>
2025-03-25 13:13:46 -0600

    feat: protect Properties view with JWT

--- api/views.py
+++ api/views.py
@@ -3,19 +3,24 @@ from rest_framework import viewsets
 from rest_framework.renderers import JSONRenderer
 from .models import *
 from .serializers import *
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 # Create your views here.
 class UserViewSet(viewsets.ModelViewSet):
     
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    renderer_classes = [JSONRenderer] 
+    renderer_classes = [JSONRenderer]
     
 class PropertyViewSet(viewsets.ModelViewSet):
     
     queryset = Property.objects.all()
     serializer_class = PropertySerializer
     renderer_classes = [JSONRenderer]
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    
     
 class RentalHistoryViewSet(viewsets.ModelViewSet):
     
